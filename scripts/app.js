bbb6e0cf7a8562ca5b6051d754db3e24bf626ed3
James Gary <gmarchesi888@gmail.com>
2019-06-24 20:24:24 -0400

    total disaster.

--- scripts/app.js
+++ scripts/app.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+//   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -22,9 +22,9 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+//   dots[slideIndex - 1].className += " active";
 }
